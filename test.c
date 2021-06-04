3d45df91d8e62126ecbca0a4c5755982cafa8695
huyhoan1109 <relifefront@gmail.com>
2021-06-04 18:42:21 +0700

    Update

--- test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int main(){
-    char line[20] = "mai ta di choi190";
-    for(int i=0;strlen(line)>i;i++){
-        if(atoi(line+i)||line[i]-'0'==0) printf("vi pham\n");
-    }
-}
